fa4bbc25537ac82aed432ce9a4b0c61564b584a0
nanaka miura <7ca.3ura@gmail.com>
2024-09-15 16:11:42 +0900

    HTML・CSSの設定

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="content">
+    <form class="create" action="" method="">
+        <div class="create-form">
+            <div class="create-form__input">
+                <input type="text">
+            </div>
+            <div class="create-form__button">
+                <button class="create-form__button--submit">作成</button>
+            </div>
+        </div>
+    </form>
+    <table class="list">
+        <tr class="list__row">
+            <th>
+                <h2 class="list__name">Todo</h2>
+            </th>
+        </tr>
+        <tr class="list__row">
+            <td class="list__update">
+                <form class="update" action="">
+                    <div class="update-form">
+                        <input class="update-form__input" type="text" value="test">
+                    </div>
+                    <div class=update-form__button>
+                        <button class="update-form__button--submit" type="submit">更新</button>
+                    </div>
+                </form>
+            </td>
+            <td class="list__delete">
+                <form class="delete" action="">
+                    <div class="delete-form__button">
+                        <button class="delete-form__button--submit" type="submit">削除</button>
+                    </div>
+                </form>
+            </td>
+        </tr>
+        <tr class="list__row">
+            <td class="list__update">
+                <form class="update" action="">
+                    <div class="update-form">
+                        <input class="update-form__input" type="text" value="test">
+                    </div>
+                    <div class=update-form__button>
+                        <button class="update-form__button--submit" type="submit">更新</button>
+                    </div>
+                </form>
+            </td>
+            <td class="list__delete">
+                <form class="delete" action="">
+                    <div class="delete-form__button">
+                        <button class="delete-form__button--submit" type="submit">削除</button>
+                    </div>
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
+@endsection
